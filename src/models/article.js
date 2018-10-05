ba0c2c73c15150cdf9ccc3cb345aecf68e18b22d
jeanmfdias <jeanmfdias@gmail.com>
2018-10-05 13:06:56 -0300

    article done

---
+++ src/models/article.js
@@ -0,0 +1,17 @@
+const restful = require('node-restful');
+
+const Media = require('./media');
+const User = require('./user');
+
+const mongoose = restful.mongoose;
+
+const Article = new mongoose.Schema({
+  title: { type: String, required: true, min: 3, max: 74 },
+  content: { type: String, required: true },
+  feature_media_id: { type: mongoose.Schema.Types.ObjectId, ref: Media },
+  slug: { type: String, required: true, min: 3, max: 100 },
+  author_user_id: { type: mongoose.Schema.Types.ObjectId, ref: User },
+  status: { type: Boolean, default: true },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now }
+});
